768937ad9c1eb20b76df9d4448ec1bcc8489587a
Rameshmanohar18 <rameshbradmandgl@gmail.com>
2026-04-17 12:11:59 +0530

    updated

--- Nodejs/Node/Day8/schemebackend/index.js
+++ Nodejs/Node/Day8/schemebackend/index.js
@@ -1,24 +1,28 @@
-// import express from 'express'
-import express from "express";
+import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import movieRoutes from './routes/movieRoute.js'
+import connectDB from './config/db.js'
+
+import userRoutes from './routes/userRoute.js'
+
 dotenv.config()
 
 const main = express()
 
+connectDB()
 
 main.use(cors())
 main.use(express.json())
 
-main.use('/api/movie',movieRoutes)
+main.use("/api/users",userRoutes)
 
 const PORT  = process.env.PORT || 3000
 
-                      
-                               
+
+
 main.listen(PORT,()=>{
     console.log(`server running on http://localhost:${PORT}`);
+    
 })
 
-// http://localhost:5000/api/movie
+// http://localhost:5000/api/users
